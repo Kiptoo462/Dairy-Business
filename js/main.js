60455182af0706bd121acf12ddf2220d73dffbe0
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-05-02 21:44:44 +0300

    Added a function that gives milk production per shed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-var record = function () {
-  var day = document.forms["daily-produce-form"]["day"].value;
-  alert(day);
+//milk production per day per shed and total amount of milk of all the shed
+//
+let record = 0;
+
+let producePerShed = new  Array(0, 0, 0, 0);
+let recordMilkPerShed = function (shedA, shedB, shedC, shedD){
+  producePerShed[0] += shedA;
+  producePerShed[1] += shedB;
+  producePerShed[2] += shedC;
+  producePerShed[3] += shedD;
+  record++;
 }
+recordMilkPerShed(510, 308, 486, 572);
+console.log(producePerShed);
+console.log(record);
+
+
+
