78b63164975286248b4ba1f2c246d841c7b1986d
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-05-02 23:04:16 +0300

    Created an object to represent months :

--- js/main.js
+++ js/main.js
@@ -56,3 +56,68 @@ let generateIncome = function (selling, time) {
 generateIncome(45, 'year');
 generateIncome(45, 'week');
 
+// income per month in a leap year
+
+
+var month = [ 
+  {
+  name: 'January', 
+  days : 31,
+  income : 0
+ },
+ {
+  name: 'February', 
+  days : 29,
+  income : 0
+ },
+ {
+  name: 'March', 
+  days : 31,
+  income : 0
+ },
+ {
+  name: 'April', 
+  days : 30,
+  income : 0
+ },
+ {
+  name: 'May', 
+  days : 31,
+  income : 0
+ },
+ {
+  name: 'June', 
+  days : 30,
+  income : 0
+ },
+ {
+  name: 'July', 
+  days : 31,
+  income : 0
+ },
+ {
+  name: 'August', 
+  days : 31,
+  income : 0
+ },
+ {
+  name: 'September', 
+  days : 30,
+  income : 0
+ },
+ {
+  name: 'October', 
+  days : 31,
+  income : 0
+ },
+ {
+  name: 'November', 
+  days : 30,
+  income : 0
+ },
+ {
+  name: 'December', 
+  days : 31,
+  income : 0
+ },
+];
