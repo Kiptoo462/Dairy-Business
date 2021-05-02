68ad11334cd70c88ae4b3bf625b387dbd19c7caa
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-05-02 23:48:06 +0300

    added a function to calculate new income and give the difference

--- js/main.js
+++ js/main.js
@@ -63,69 +63,92 @@ var month = [
   {
   name: 'January', 
   days : 31,
-  income : 0
+  income : 0,
+  newIncome : 0
+  
  },
  {
   name: 'February', 
   days : 29,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'March', 
   days : 31,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'April', 
   days : 30,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'May', 
   days : 31,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'June', 
   days : 30,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'July', 
   days : 31,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'August', 
   days : 31,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'September', 
   days : 30,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'October', 
   days : 31,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'November', 
   days : 30,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
  {
   name: 'December', 
   days : 31,
-  income : 0
+  income : 0,
+  newIncome : 0
  },
 ];
 
 let getIncomePerMonth = function () {
   for( i = 0; i<12; i++) {
     month[i].income = 45 * averagePerDay() * month[i].days;
-    console.log('Your income for ' + month[i].name + ' is ' + month[i].income ) 
+    console.log('Your income for ' + month[i].name + ' is ' + month[i].income );
   }
 }
 getIncomePerMonth();
+ 
+let getNewIncomePerMonth = function () {
+  for( i = 0; i<12; i++) {
+    month[i].newIncome = 49.60 * averagePerDay() * month[i].days;
+    console.log( 'your new income for ' + month[i].name + ' is ' + month[i].newIncome);
+    console.log( ' The diffrence in income is ' + ( month[1].newIncome - month[i].income));
+  }
+}
+ 
+getNewIncomePerMonth();
