292fc2dbfff24ae1341a56ae79b50c8e1713a3bd
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-05-02 22:55:41 +0300

    Added a function to calculate weekly and yearly icome

--- js/main.js
+++ js/main.js
@@ -17,16 +17,42 @@ console.log(record);
 //total amount of milk of all the sheds
 
  let averagePerDay = function () {
-   let averageShedA = producePerShed[0]/record;
+   let averageShedA = Math.ceil(producePerShed[0]/record);
    console.log('Your production in shed A ' + averageShedA + ' litres per day');
-   let averageShedB = producePerShed[1]/record;
+   let averageShedB = Math.ceil(producePerShed[1]/record);
    console.log('Your production in shed B ' + averageShedB + ' litres per day');
-   let averageShedC = producePerShed[2]/record;
+   let averageShedC = Math.ceil(producePerShed[2]/record);
    console.log('Your production in shed C ' + averageShedC + ' litres per day');
-   let averageShedD = producePerShed[3]/record;
+   let averageShedD = Math.ceil(producePerShed[3]/record);
    console.log('Your production in shed D ' + averageShedD + ' litres per day');
    let averageProducePerDay= averageShedA+averageShedB+averageShedC+averageShedD;
    console.log('Your total production is ' + averageProducePerDay + ' litres per day');
+   return averageProducePerDay;
   }
   
 averagePerDay();
+ 
+//weekly  and yearly income 
+let generateIncome = function (selling, time) {
+  let days = 0;
+  let incomePerWeek = 0;
+  let incomePerYear = 0;
+  if (time == 'week'){
+    days = 7;
+    incomePerWeek = averagePerDay() * selling * days;
+    console.log('Your weekly income will be ksh ' + incomePerWeek);
+  }
+  else if (time =='year'){
+    days = 365;
+    incomePerYear = averagePerDay() * selling * days;
+    console.log('Your yearly income will be ksh ' + incomePerYear);
+  
+  }
+  else { 
+    alert('Invalid time');
+  }
+ 
+}
+generateIncome(45, 'year');
+generateIncome(45, 'week');
+
